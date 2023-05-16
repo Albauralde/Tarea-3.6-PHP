9f80342a6ab9930fdb55ae19308c732697bd372f
Albauralde <125832032+Albauralde@users.noreply.github.com>
2023-05-16 19:37:40 +0200

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,69 @@
+<!doctype html>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Formulario de Registro SCIII</title>
+<link href="estilos.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="group">
+
+    <form method="POST" action="">
+    <h2><em>Formulario de Registro</em></h2><br />
+<label for="nombre">Nombre <span><em>(requerido)</em></span></label>
+<input type="text" name="nombre" class="form-input" required/><br /><br /> 
+
+<label for="apellido"> Apellido <span><em>(requerido)</em></span></label>
+<input type="text" name="apellido" class="form-input" required/><br /><br /> 
+
+<label for="email"> Email <span><em>(requerido)</em></span></label>
+<input type="email" name="email" class="form-input" /><br /><br /> 
+
+<input class="form-btn" name="submit" type="submit" value="Suscribirse" /><br /><br /> 
+
+</form>
+
+<?php
+    if($_POST){
+        $nombre=$_POST['nombre'];
+        $apellido=$_POST['apellido'];
+        $email=$_POST['email'];
+
+    //Conexion con PDO
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cursosql";
+
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+   
+    //Check connection
+    if ($conn->connect_error) {
+        die("Connection failed; " . $conn-> connect_error);
+    }
+
+    $sql = "INSERT INTO usuario (nombre, apellido, email)
+    VALUES ('$nombre', '$apellido', '$email')";
+
+    if ($conn->query($sql) === TRUE){
+        echo "New record created succesfully";
+    } else {
+        echo "Error" . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+
+    }
+
+    ?>
+
+    
+
+</div>
+</body>
+</html>
+
+    
